51748a2e37c4dfd14c6d25438eb20d25f73e0b3b
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-08-29 11:40:13 -0500

    cxx 11 to cxx 14. Ensure gtest/gmock works

--- README.md
+++ README.md
@@ -3,14 +3,3 @@
 Adapted from TDD w/ Modern C++ (Jeff Langr)
 
 ---
-
-## Problems
-
-While debugging with a breakpoint:
-
-```vscode
-Unable to open 'libc-start.c': Unable to read file
-'/build/glibc-eX1tMB/glibc-2.31/csu/libc-start.c'
-(Error: Unable to resolve nonexistent file
-'/build/glibc-eX1tMB/glibc-2.31/csu/libc-start.c').
-```
--- gtest_eg.cc
+++ gtest_eg.cc
@@ -1,25 +1,34 @@
+#include <gmock/gmock.h>
+// #include <gtest/gtest.h>
 #include <string>
+
+// using ::testing::Eq;
+// using namespace testing;
+
 class Soundexer {
 public:
-  std::string encode(const std::string &word) const { return ""; }
+  std::string encode(const std::string &word) const { return pad_zeroes(word); }
+
+private:
+  std::string pad_zeroes(const std::string &word) const { return word + "000"; }
 };
 
-#include <gtest/gtest.h>
+class SoundexerFixture : public testing::Test {
+public:
+  Soundexer sdxr;
+};
+
+TEST_F(SoundexerFixture, FirstCharacter) {
+  auto encoded = sdxr.encode("A");
+  ASSERT_THAT(encoded, testing::Eq("A"));
+}
+
+TEST_F(SoundexerFixture, PadThreeDigits) {
+  auto encoded = sdxr.encode("I");
+  ASSERT_THAT(encoded, testing::Eq("I000"));
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
-TEST(SoundexerEncoder, FirstCharacter) {
-  Soundexer sdxr;
-  auto encoded = sdxr.encode("A");
-}
-
-TEST(HelloTest, BasicAssertions) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
-}
-
-// cmake -S . -B build
-// cmake --build build
