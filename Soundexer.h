55e8bf07d9c3882d1246135ae1312af968593e24
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-08-31 22:02:58 -0500

    Small refactors

--- Soundexer.h
+++ Soundexer.h
@@ -1,21 +1,29 @@
 #ifndef Soundexer_h
 #define Soundexer_h
+#include <cstddef>
 #include <iostream>
 #include <string>
 
 class Soundexer {
 public:
+  static const std::size_t min_word_length{4};
   std::string encode(const std::string &word) const {
-    auto encoded = word.substr(0, 1);
-    std::cout << encoded;
-    if (word.length() > 1) {
-      encoded += "1";
-    }
-    return pad_zeroes(encoded);
+    return pad_zeroes(zero_index_of(word) + encoded_digits(word));
   }
 
 private:
-  const unsigned char min_word_length = 4;
+  std::string zero_index_of(const std::string &word) const {
+    return word.substr(0, 1);
+  }
+
+  std::string encoded_digits(const std::string &word) const {
+    if (word.length() > 1) {
+      return "1";
+    } else {
+      return "";
+    }
+  }
+
   std::string pad_zeroes(const std::string &word) const {
     auto zeroes_needed = min_word_length - word.length();
     return word + std::string(zeroes_needed, '0');
