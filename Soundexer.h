755903acab99b7479bf281fb06469b26e859c699
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-26 03:16:38 -0600

    Map encoding, ignore non-alphabet letters

--- Soundexer.h
+++ Soundexer.h
@@ -9,29 +9,32 @@ class Soundexer {
 public:
   static const std::size_t min_word_length{4};
   std::string encode(const std::string &word) const {
-    return pad_zeroes(zero_index_of(word) + encoded_digits(word));
+    return pad_zeroes(head(word) + encoded_to_digits(word));
+    // return pad_zeroes(head(word)) + encoded_to_digits(tail(word));
   }
 
 private:
-  std::string zero_index_of(const std::string &word) const {
-    return word.substr(0, 1);
-  }
+  std::string head(const std::string &word) const { return word.substr(0, 1); }
+  // std::string tail(const std::string &word) const { return word.substr(1); }
 
-  std::string encoded_digit(char letter) const {
-    const std::unordered_map<char, std::string> encodings{
-        {'b', "1"},
-        {'c', "2"},
-        {'d', "3"},
+  std::string encoded_to_digit(char letter) const {
+    const std::unordered_map<char, std::string> mapped_encodings{
+        // https://en.wikipedia.org/wiki/Soundex
+        {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"}, {'c', "2"}, {'g', "2"},
+        {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+        {'d', "3"}, {'t', "3"}, {'l', "4"}, {'m', "5"}, {'n', "5"}, {'r', "6"},
     };
-    return encodings.find(letter)->second;
+    auto found_it = mapped_encodings.find(letter);
+    // Check if find() points to end(), returning an empty string
+    // If not, return the encoded (second) value what was found
+    return found_it == mapped_encodings.end() ? "" : found_it->second;
   }
 
-  std::string encoded_digits(const std::string &word) const {
-    if (word.length() > 1) {
-      return encoded_digit(word[1]);
-    } else {
-      return std::string();
-    }
+  std::string encoded_to_digits(const std::string &word) const {
+    std::string encoded;
+    for (auto letter : word)
+      encoded += encoded_to_digit(letter);
+    return encoded;
   }
 
   std::string pad_zeroes(const std::string &word) const {
--- gtest_eg.cc
+++ gtest_eg.cc
@@ -1,5 +1,6 @@
 #include "Soundexer.h"
-#include <gmock/gmock.h>
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include <string>
 
 // using ::testing::Eq;
@@ -15,16 +16,26 @@ TEST_F(SoundexerFixture, first_character) {
   ASSERT_THAT(encoded, testing::Eq("A000"));
 }
 
-TEST_F(SoundexerFixture, pad_three_digits) {
+TEST_F(SoundexerFixture, pad_to_three_digits) {
   auto encoded = sdxr.encode("abc");
   ASSERT_THAT(encoded.length(), testing::Eq(4));
 }
 
-TEST_F(SoundexerFixture, consonant_numeral) {
+TEST_F(SoundexerFixture, consonant_to_numeral) {
   EXPECT_THAT(sdxr.encode("Ab"), testing::Eq("A100"));
   EXPECT_THAT(sdxr.encode("Ac"), testing::Eq("A200"));
   EXPECT_THAT(sdxr.encode("Ad"), testing::Eq("A300"));
+  // No need to test everything, considering the functionality will be identical
+  EXPECT_THAT(sdxr.encode("Ax"), testing::Eq("A200"));
 }
+
+TEST_F(SoundexerFixture, ignore_non_alphabet) {
+  ASSERT_THAT(sdxr.encode("A#"), testing::Eq("A000"));
+}
+
+// TEST_F(SoundexerFixture, many_consonants_to_numerals) {
+//   ASSERT_THAT(sdxr.encode("Acdl"), testing::Eq("A234"));
+// }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
